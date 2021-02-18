aa02e04b7f4df6b4cf03180d5559696576367566
Evelin <evelinzirakashvili11@gmail.com>
2021-02-18 17:12:00 +0400

    Fetch data and create product's list

---
+++ src/components/ProductList.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="container d-flex">
+    <div v-for="product in products" :key="product.id">
+      <SingleProduct :product="product" />
+    </div>
+  </div>
+</template>
+
+<script>
+import SingleProduct from './SingleProduct'
+export default {
+  components: { SingleProduct },
+  computed: {
+    products() {
+      return this.$store.state.products
+    },
+  },
+  mounted() {
+    this.$store.dispatch('getProducts')
+  },
+}
+</script>
+
+<style>
+.container {
+  max-width: 1200px;
+  margin: 0 auto;
+}
+.d-flex {
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-start;
+  flex-wrap: wrap;
+}
+</style>
---
+++ src/components/SingleProduct.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="cart">
+    <router-link
+      class="link"
+      :to="{ name: 'Product', params: { id: product.id } }"
+    >
+      <img class="img" :src="product.image" :alt="product.name" />
+      <h1 class="title">{{ product.title }}</h1>
+    </router-link>
+    <p class="description">{{ snippet }}</p>
+    <p class="price">Price: {{ product.price }}</p>
+    <button class="btn addcard">Add To cart</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['product'],
+  computed: {
+    snippet() {
+      return this.product.description.substring(0, 100) + '...'
+    },
+  },
+}
+</script>
+
+<style>
+.cart {
+  width: 300px;
+  background: #fff4f4;
+  height: auto;
+  padding: 15px;
+  border-radius: 20px;
+  margin: 30px 10px 0px 10px;
+}
+.cart .img {
+  width: 270px;
+  border-radius: 5px;
+}
+.cart .description {
+  margin: 10px 0;
+}
+.title {
+  text-decoration: none;
+  color: #f05627;
+  cursor: pointer;
+  font-size: 25px;
+  transition-duration: 0.3s;
+}
+.link {
+  text-decoration: none;
+}
+.title:hover {
+  color: crimson;
+}
+.addcard {
+  margin: 5px 0;
+  background: #f05627;
+  color: #fff;
+}
+</style>
