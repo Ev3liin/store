690f1e35c974cc175b60b7069d9351de28277d5b
Evelin <evelinzirakashvili11@gmail.com>
2021-02-18 17:07:27 +0400

    Create navigation and add to the App.vue

---
+++ src/components/Navbar.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="nav">
+    <div class="container d-flex">
+      <div>
+        <router-link :to="{ name: 'Home' }">Home</router-link>
+        <!-- <router-link :to="{ name: 'Product' }">Product</router-link> -->
+      </div>
+      <MiniCart />
+    </div>
+  </div>
+</template>
+
+<script>
+import Home from '../views/Home'
+import Product from '../views/Product'
+import MiniCart from './MiniCart'
+export default {
+  components: { Home, Product, MiniCart },
+}
+</script>
+
+<style scoped>
+.container {
+  max-width: 1200px;
+  margin: 0 auto;
+}
+.d-flex {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+.nav {
+  height: 6vh;
+  background: #f3e2c8;
+  padding: 8px 0;
+}
+.logo {
+  font-size: 30px;
+  font-weight: bold;
+  color: #f05627;
+}
+.nav a {
+  font-weight: bold;
+  color: #7e92a7;
+  text-decoration: none;
+  padding: 8px;
+  border-radius: 10px;
+}
+
+.nav a.router-link-exact-active {
+  background: #f05627;
+  color: #fff;
+}
+</style>
