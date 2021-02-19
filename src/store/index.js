3298b2a019338fcde0d1f065db45f09afd3e302a
Evelin <evelinzirakashvili11@gmail.com>
2021-02-19 11:01:38 +0400

    Remove functionality from the store and  import module

--- src/store/index.js
+++ src/store/index.js
@@ -1,27 +1,8 @@
 import { createStore } from 'vuex'
+import productsModule from './module/products'
 
 export default createStore({
-  state: {
-    products: [],
-    peoduct: null,
+  modules: {
+    products: productsModule,
   },
-  mutations: {
-    GET_PRODUCTS(state, products) {
-      state.products = products
-    },
-    GET_PRODUCT(state, product) {
-      state.product = product
-    },
-  },
-  actions: {
-    async getProducts({ commit }) {
-      const res = await fetch('http://localhost:3000/products')
-      commit('GET_PRODUCTS', await res.json())
-    },
-    async getProduct({ commit }, productId) {
-      const res = await fetch(`http://localhost:3000/products/${productId}`)
-      commit('GET_PRODUCT', await res.json())
-    },
-  },
-  modules: {},
 })
